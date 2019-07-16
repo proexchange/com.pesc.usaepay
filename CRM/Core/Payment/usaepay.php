64c5e3a063404388ec5169a0908fed096ddf40d3
netzih <48005023+netzih@users.noreply.github.com>
2019-07-16 13:59:38 -0700

    Update usaepay.php

--- CRM/Core/Payment/usaepay.php
+++ CRM/Core/Payment/usaepay.php
@@ -119,7 +119,7 @@ class CRM_Core_Payment_usaepay extends CRM_Core_Payment {
       $tran->gatewayurl = $paymentProcessor['url_recur'];
       $tran->addcustomer  = "yes";
       $tran->numleft  = $params['installments'];
-      $tran->start = date('Ymd');
+      $tran->start = 'next';
       $tran->custid = $params['contributionRecurID'];
       //recurring schedule
       switch ($params['frequency_unit']) {
